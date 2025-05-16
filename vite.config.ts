282742a636499e10df71fd438df0d6d0ee6501b5
kubaparol <kubciop33@gmail.com>
2025-05-16 11:10:08 +0200

    feat: Update project version to 0.0.1, add vite-plugin-image-optimizer, and configure Vite settings. Include APP_VERSION in the app and clean up TypeScript configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import packageJson from "./package.json";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    minify: true,
+    sourcemap: false,
+  },
+  define: {
+    APP_VERSION: JSON.stringify(packageJson.version),
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+    }),
+  ],
+});
